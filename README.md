3196d16257f4a88e1265ff4e83cb7836f0d0b95b
zhuweiyou <i@zhuweiyou.com>
2023-08-30 17:15:58 +0800

    public

---
+++ README.md
@@ -0,0 +1,15 @@
+# excel-to-json
+
+一键转换 excel 为 json, 支持多 sheet
+
+## 使用说明
+
+下载你的平台对应的可执行文件 [release]()
+
+鼠标拖动 excel 文件到本程序图标上, 即可完成转换
+
+或命令:
+
+```
+./excel-to-json 文件路径.csv
+```
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/xuri/excelize/v2"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	defer func() {
+		os.Stdin.Read(make([]byte, 1))
+	}()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: https://github.com/zhuweiyou/excel-to-json")
+		return
+	}
+
+	from := os.Args[1]
+	fmt.Printf("From: %s\n", from)
+
+	file, err := excelize.OpenFile(from)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %v\n", from, err)
+		return
+	}
+	defer file.Close()
+
+	sheetList := file.GetSheetList()
+	fmt.Printf("Sheet list: %v\n", sheetList)
+
+	for index, sheet := range file.GetSheetList() {
+		fmt.Printf("Sheet (%d/%d): %s\n", index+1, len(sheetList), sheet)
+		rows, err := file.GetRows(sheet)
+		if err != nil {
+			fmt.Printf("Error getting rows: %v\n", err)
+			return
+		}
+		fmt.Printf("Rows: %d\n", len(rows))
+
+		marshal, err := json.Marshal(rows)
+		if err != nil {
+			fmt.Printf("Error marshaling rows: %v\n", err)
+			return
+		}
+
+		dir, filename := filepath.Split(from)
+		to := filepath.Join(dir, filename[:len(filename)-len(filepath.Ext(from))]+"_"+sheet+".json")
+		err = os.WriteFile(to, marshal, 0666)
+		if err != nil {
+			fmt.Printf("Error writing to %v: %v\n", to, err)
+			return
+		}
+
+		fmt.Printf("Successfully converted to %v\n", to)
+	}
+}
